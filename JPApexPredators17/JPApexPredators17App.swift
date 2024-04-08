fcee13e77fc525cce83726fd7e17841ed09a62f9
alcode111 <al.code.111@gmail.com>
2024-04-08 11:52:31 +0200

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Ismaïl LP on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
