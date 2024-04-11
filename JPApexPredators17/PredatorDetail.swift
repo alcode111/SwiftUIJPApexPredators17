69bf44f062bfa3776748ef2b30bd4f4081c327c0
alcode111 <al.code.111@gmail.com>
2024-04-11 14:55:29 +0200

    Add an Annotation and a NavigationLink

--- JPApexPredators17/PredatorDetail.swift
+++ JPApexPredators17/PredatorDetail.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct PredatorDetail: View {
     let predator: ApexPredator
+    @State var position: MapCameraPosition
     
     var body: some View {
         GeometryReader { geo in
@@ -40,8 +42,43 @@ struct PredatorDetail: View {
                 VStack(alignment: .leading) {
                     Text(predator.name)
                     
+                    NavigationLink {
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                    } label: {
+                        Map(position: $position) {
+                            Annotation(
+                                predator.name,
+                                coordinate: predator.location) {
+                                    Image(systemName: "mappin.and.ellipse")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .symbolEffect(.pulse)
+                                }
+                                .annotationTitles(.hidden)
+                        }
+                        .frame(height: 125)
+                        .overlay(alignment: .trailing) {
+                            Image(systemName: "greaterthan")
+                                .imageScale(.large)
+                                .font(.title3)
+                                .padding(.trailing, 5)
+                        }
+                        .overlay(alignment: .topLeading) {
+                            Text("Current Location")
+                                .padding([.leading, .bottom], 5)
+                                .padding(.trailing, 8)
+                                .background(.black.opacity(0.33))
+                                .clipShape(.rect(bottomTrailingRadius: 15))
+                        }
+                        .clipShape(.rect(cornerRadius: 15))
+
+                    }
+                    
                     Text("Appears In:")
                         .font(.title3)
+                        .padding(.top)
                     
                     ForEach(predator.movies, id: \.self) { movie in
                         Text("•" + movie)
@@ -74,10 +111,16 @@ struct PredatorDetail: View {
             }
             .ignoresSafeArea()
         }
+        .toolbarBackground(.automatic)
     }
 }
 
 #Preview {
-    PredatorDetail(predator: Predators().apexPredators[10])
+    NavigationStack {
+        PredatorDetail(predator: Predators().apexPredators[2],
+                       position: .camera(MapCamera(
+                        centerCoordinate: Predators().apexPredators[2].location,
+                        distance: 30000)))
         .preferredColorScheme(.dark)
+    }
 }
